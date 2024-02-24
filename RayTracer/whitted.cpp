7bc56eff1cccf210d470848e5f16cb3bc003fa7c
Mason Suess <masonsuess@gmail.com>
2024-02-23 20:18:13 -0500

    Update whitted.cpp

--- RayTracer/whitted.cpp
+++ RayTracer/whitted.cpp
@@ -1,31 +1,4 @@
-//[header]
-// A simple program to demonstrate how to implement Whitted-style ray-tracing
-//[/header]
-//[compile]
-// Download the whitted.cpp file to a folder.
-// Open a shell/terminal, and run the following command where the files is saved:
-//
-// c++ -o whitted whitted.cpp -std=c++11 -O3
-//
-// Run with: ./whitted. Open the file ./out.png in Photoshop or any program
-// reading PPM files.
-//[/compile]
-//[ignore]
-// Copyright (C) 2012  www.scratchapixel.com
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//[/ignore]
+
 
 
 #include <cstdio>
